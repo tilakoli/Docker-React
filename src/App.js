1546fe9c8b2acb8e54a6cfe0b75333e83c6ab874
Tilak Oli <tilak.81oli@gmail.com>
2022-09-04 10:26:42 +0545

    Readme & app.js file updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div>
+        <h1 className="Content"> Dockerize a React App !</h1>
+        <p className="desc">
+          Dockerizing a react app for development with live reloading for react
+          apps.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
